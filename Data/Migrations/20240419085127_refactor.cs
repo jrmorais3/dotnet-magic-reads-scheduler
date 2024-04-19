ca72b24cee22050a2915a774843d1dab281dd1ed
Junior Morais <franciscojunior@employer.com.br>
2024-04-19 05:53:12 -0300

    refactor database and EF Core upgrade

---
+++ Data/Migrations/20240419085127_refactor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SFData.Migrations
+{
+    /// <inheritdoc />
+    public partial class refactor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "Voluntarios",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "DtaNascimento",
+                table: "Voluntarios",
+                newName: "Birthday");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Voluntarios",
+                newName: "Nome");
+
+            migrationBuilder.RenameColumn(
+                name: "Birthday",
+                table: "Voluntarios",
+                newName: "DtaNascimento");
+        }
+    }
+}
