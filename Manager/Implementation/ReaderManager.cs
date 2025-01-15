18316f0ca0873baf454e271d8f35fbeab6233fd7
Junior Morais <jrmorais3@gmail.com>
2025-01-15 19:16:22 -0300

    refactor: rename manager\*

---
+++ Manager/Implementation/ReaderManager.cs
@@ -0,0 +1,27 @@
+﻿using Core.Domain;
+using Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Implementation
+{
+    public class ReaderManager : IReaderManager
+    {
+        private readonly IReaderRepository voluntarioRepository;
+
+        public ReaderManager(IReaderRepository ReaderRepository)
+        {
+            this.voluntarioRepository = ReaderRepository;
+        }
+
+        public async Task<IEnumerable<Reader>> GetReaderAsync()
+        {
+            return await voluntarioRepository.GetReadersAsync();
+        }
+
+        public async Task<Reader> GetReaderAsync(int id)
+        {
+            return await voluntarioRepository.GetReaderAsync(id);
+        }
+    }
+}
---
+++ Manager/Interfaces/IReaderManager.cs
@@ -0,0 +1,12 @@
+﻿using Core.Domain;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IReaderManager
+    {
+        Task<Reader> GetReaderAsync(int id);
+        Task<IEnumerable<Reader>> GetReaderAsync();
+    }
+}
