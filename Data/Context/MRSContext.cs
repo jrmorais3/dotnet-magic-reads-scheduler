9231c0d6b1cf8cd4153efb3a988cb9293bc2f3b3
Junior Morais <jrmorais3@gmail.com>
2025-01-15 19:15:22 -0300

    refactor: rename data\context

---
+++ Data/Context/MRSContext.cs
@@ -0,0 +1,10 @@
+﻿using Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Context
+{
+    public class MRSContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Reader> Voluntarios { get; set; }
+    }
+}
