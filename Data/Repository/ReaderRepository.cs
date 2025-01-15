b355d901880cc90d6bbb5b9bcf12db4321951057
Junior Morais <jrmorais3@gmail.com>
2025-01-15 19:16:01 -0300

    refactor: rename data\repository

--- Data/Repository/ReaderRepository.cs
+++ Data/Repository/ReaderRepository.cs
@@ -7,21 +7,21 @@ using System.Threading.Tasks;
 
 namespace Repository
 {
-    public class CustomerRepository : ICustomerRepository
+    public class ReaderRepository : IReaderRepository
     {
-        private readonly CSContext context;
+        private readonly MRSContext context;
 
-        public CustomerRepository(CSContext context)
+        public ReaderRepository(MRSContext context)
         {
             this.context = context;
         }
 
-        public async Task<IEnumerable<Customer>> GetCustomersAsync()
+        public async Task<IEnumerable<Reader>> GetReadersAsync()
         {
             return await context.Voluntarios.AsNoTracking().ToListAsync();
         }
 
-        public async Task<Customer> GetCustomerAsync(int id)
+        public async Task<Reader> GetReaderAsync(int id)
         {
             return await context.Voluntarios.FindAsync(id);
         }
